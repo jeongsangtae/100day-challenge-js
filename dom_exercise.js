a13e1f03d733cb7492fafdebc8e0467eb71f0035
jeongsangtae <wjdtkdxo55@naver.com>
2023-01-21 22:16:48 +0900

    dom exercise finish

--- dom_exercise.js
+++ dom_exercise.js
@@ -8,13 +8,19 @@ console.dir(first);
 // 2. Use the variable from (1) and get access to the "parent"
 //    element of the stored <h1> element (i.e. to the <body> element)
 
+console.dir(first.parentElement);
+
 //    BONUS: Try using the variable from (1) to get access to the
 //    sibling element (i.e. the <p> element next to the <h1> element)
+
+console.dir(first.nextElementSibling);
 
 // 3. Select the <h1> element with getElementById and store in
 //    the same or a new variable (up to you)
 
 first = document.getElementById("hi");
+
+console.dir(first);
 
 // 4. Select the second <p> element with querySelector (you might
 //    need to add something in the HTML code, e.g. a class)
@@ -22,7 +28,11 @@ first = document.getElementById("hi");
 
 let second = document.querySelector(".link");
 
+console.dir(second);
+
 // 5. BONUS TASK: Try changing the text content of the <p> element
 //    you selected in (4) and set it to any other text of your choice
 
-second.innerHTML = "It is a Links";
+// second.innerHTML = "It is a Links";
+
+second.textContent = "It is a Links";
